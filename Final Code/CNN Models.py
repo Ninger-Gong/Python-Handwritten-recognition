072717555ec134f66c2f6b54fc59af3006137d76
Ninger-Gong <61758760+Ninger-Gong@users.noreply.github.com>
2020-05-05 11:48:28 +1200

    Update CNN Models.py

--- Final Code/CNN Models.py
+++ Final Code/CNN Models.py
@@ -1,27 +1,27 @@
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(
+        self.Convlayer1 = nn.Sequential(
             nn.Conv2d(1, 25, kernel_size=3),
             nn.BatchNorm2d(25),
             nn.ReLU(inplace=True)
         )
 
-        self.layer2 = nn.Sequential(
+        self.Maxplayer2 = nn.Sequential(
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
-        self.layer3 = nn.Sequential(
+        self.Convlayer3 = nn.Sequential(
             nn.Conv2d(25, 50, kernel_size=3),
             nn.BatchNorm2d(50),
             nn.ReLU(inplace=True)
         )
 
-        self.layer4 = nn.Sequential(
+        self.Maxplayer4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
-        self.fc = nn.Sequential(
+        self.FC = nn.Sequential(
             nn.Linear(50 * 5 * 5, 1024),
             nn.ReLU(inplace=True),
             nn.Linear(1024, 128),
@@ -30,10 +30,10 @@ class CNN(nn.Module):
         )
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x = self.Convlayer1(x)
+        x = self.Maxplayer2(x)
+        x = self.Convlayer3(x)
+        x = self.Maxplayer4(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.FC(x)
         return x
