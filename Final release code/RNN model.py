e5ad6078aad77959930756f46e10cf7489008c27
Bear-Xie <62578995+Bear-Xie@users.noreply.github.com>
2020-05-12 17:56:44 +0800

    Update RNN model.py

--- Final release code/RNN model.py
+++ Final release code/RNN model.py
@@ -1,77 +1,89 @@
 import torch
-import time
+import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-import matplotlib.pyplot as plt
+import torchvision
+from utils import plot_image, plot_curve
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
-from torch.autograd import Variable
 
-def plot_curve(data):
-    fig = plt.figure()
-    plt.plot(range(len(data)), data, color = 'blue')
-    plt.legend(['value'], loc = 'upper right')
-    plt.xlabel('step')
-    plt.ylabel('value')
-    plt.show()
+#load data input
+batch_size = 512
+num_epochs = 20
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('data', train=True, download= True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (0.1307,), (0.3081, ))
+                               ])),
+    batch_size=batch_size, shuffle=True)
 
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('data', train=False, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (0.1307,), (0.3081,))
+                               ])),
+    batch_size=batch_size, shuffle=False)
 
-def plot_image(img, label, name):
-    fig = plt.figure()
-    for i in range(6):
-        plt.subplot(2, 3, i+1)
-        plt.tight_layout()
-        plt.imshow(img[i][0]*0.3081+0.1307, cmap='gray', interpolation= 'none')
-        plt.title("{}: {}".format(name, label[i].item()))
-        plt.xticks([])
-        plt.yticks([])
-    plt.show()
+x, y = next(iter(train_loader))
+print(x.shape, y.shape, x.min(), x.max())
+plot_image(x, y, 'Real Value')
 
+# construct net work model
+class RNN(nn.Module):
+    def __init__(self, input, hidden, layer):
+        super(RNN, self).__init__()
+        self.hidden = hidden
+        self.layer = layer
+        self.rnn = nn.RNN(input, hidden, layer, batch_first=True, nonlinearity='tanh')
+        self.fc = nn.Linear(hidden,10)
 
-def one_hot(label, depth = 10):
-    out = torch.zeros(label.size(0), depth)
-    idx = torch.LongTensor(label).view(-1, 1)
-    out.scatter_(dim = 1, index = idx, value = 1)
-    return out
+    def forward(self, x):
+        h0 = torch.zeros(self.layer, x.size(0), self.hidden).requires_grad_()
+        out, hn = self.rnn(x, h0.detach())
+        out = self.fc(out[:, -1, :])
 
+        return out
 
-def infer(model, inputs):
-    if torch.cuda.is_available():
-        inputs = inputs.cuda()
-    x_batch = Variable(inputs, volatile=True, requires_grad=False)
+# model and optimizer
+model = RNN(28, 100, 2)
 
-    scores = model(x_batch)
+loss_func = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
-    scores, predicted = torch.max(scores, 1)
+# train and test
+iteration = 0
+for epoch in range(num_epochs):
+    for step, (images, targets) in enumerate(train_loader):
+        images = images.view(-1, 28, 28).requires_grad_()
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = loss_func(outputs, targets)
+        loss.backward()
+        optimizer.step()
 
-    return predicted
+        iteration += 1
 
-# Visualize some samples
-def visualize(test_loader):
-    batch = next(iter(test_loader))
-    samples = batch[0][:5]
-    y_true = batch[1]
-    for i, sample in enumerate(samples):
-        plt.subplot(1, 5, i+1)
-        plt.title('Numbers: %i' % y_true[i])
-        plt.imshow(sample.numpy().reshape((28, 28)))
-        plt.axis('off')
+        if step % 500 == 0:
+            correct = 0
+            total = 0
+            p1 = 0  # Precision Rate
+            p2 = 0
+            r1 = 0  # Recall Rate
+            r2 = 0
+            f1 = 0
+            for images, targets in test_loader:
+                images = images.view(-1, 28,28)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1) # find the target with the most possibility
+                total += targets.size(0)
+                correct += (predicted == targets).sum()
+            accuracy = float(100 * correct / total)
+            print('Iteration: {}. Loss: {:.3f}%. Accuracy: {:.3f}%'.format(iteration, loss.item(), accuracy))
 
-from PIL import Image
-from torch.autograd import Variable
-from torchvision import datasets, transforms
-
-def image_loader(filename):
-    # load the image and return the cuda tensor
-    image = Image.open(filename)
-    loader = transforms.Compose([
-        transforms.Resize(256),
-        transforms.ToTensor()
-    ])
-    image = loader(image).float()
-    image = Variable(image,requires_grad=True)
-    image = image.unsqueeze(0)
-    return image
-
+data, targets = next(iter(test_loader))
+out = RNN(data.view(data.size(0), 28*28),100,2)
+pred = out.argmax(dim = 1)
+plot_image(data, pred, 'Prediction')
